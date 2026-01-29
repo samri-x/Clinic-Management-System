b579e1d5a9c637c3a82c2eff2f26a4c1924418aa
RuthK <ruthkumie@gmail.com>
2026-01-29 18:39:27 +0300

    Create AdminLogin.java

---
+++ src/main/java/clinic/logic/AdminLogin.java
@@ -0,0 +1,16 @@
+package clinic.logics;
+
+import clinic.data.DataStore;
+
+public class AdminLogic {
+
+    public static String getCounts() {
+        return "Doctors: " + DataStore.doctors.size() +
+                "   |   Patients: " + DataStore.patients.size() +
+                "   |   Appointments: " + DataStore.appointments.size();
+    }
+
+    public static void refreshCountsLabel(javafx.scene.control.Label countsLabel) {
+        countsLabel.setText(getCounts());
+    }
+}
